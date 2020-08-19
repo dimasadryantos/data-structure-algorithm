b1872f00e915eccda79a294711eafb15177728cc
dimasadriyantos@gmail.com <dimasdz99>
2020-08-19 12:12:31 +0700

    bubble sort

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,16 +3,26 @@ package com.main;
 public class Main {
 
     public static void main(String[] args) {
+        descendingSorting();
+        ascendingSorting();
+    }
 
+
+
+
+    /**
+     * bubble sorting bigger to small
+     */
+    private static void descendingSorting() {
 
         int[] intArray = {20, 35, 15, 7, 55, 1, 14};
 
-        for (int lastUnsortedIndex = 0;lastUnsortedIndex < intArray.length; lastUnsortedIndex++) {
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 
-            for (int i = intArray.length -1; i > lastUnsortedIndex; i--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
 
-                if (intArray[i] > intArray[i - 1]) {
-                    swap(intArray, i, i - 1);
+                if (intArray[i] < intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
                 }
             }
         }
@@ -20,8 +30,33 @@ public class Main {
         for (int i = 0; i < intArray.length; i++) {
             System.out.print(" " + intArray[i]);
         }
-
     }
+
+
+
+
+    /**
+     * bubble sorting small to bigger
+     */
+    private static void ascendingSorting() {
+
+        int[] arrayInt = {5, 12, 26, 2, 45, 3};
+
+        for (int unsortList = arrayInt.length - 1; unsortList > 0; unsortList--) {
+
+            for (int i = 0; i < unsortList; i++) {
+
+                if (arrayInt[i] > arrayInt[i + 1]) {
+                    swap(arrayInt, i, i + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print(" " + arrayInt[i]);
+        }
+    }
+
 
 
     private static void swap(int[] array, int i, int j) {
@@ -35,6 +70,5 @@ public class Main {
         array[j] = temp;
     }
 
-    //OUTPUT SHOULD BE :  55 35 20 15 14 7 1
 
 }
