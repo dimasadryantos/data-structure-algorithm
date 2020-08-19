d381b9733e54d2e966108013c9c55682ce87eca5
dimasadriyantos@gmail.com <dimasdz99>
2020-08-19 16:30:30 +0700

    selection sort and update method name

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,24 +3,39 @@ package com.main;
 public class Main {
 
     public static void main(String[] args) {
-        descendingSorting();
-        ascendingSorting();
+
+
     }
 
+    private static void selectionShortAscending() {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
+        for (int lastSortedIdex = intArray.length - 1; lastSortedIdex > 0; lastSortedIdex--) {
 
+            int largest = 0;
+            for (int i = 1; i <= lastSortedIdex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+            swap(intArray, largest, lastSortedIdex);
+        }
+
+        for(int i =0;i < intArray.length;i++){
+            System.out.println(intArray[i]);
+        }
+    }
 
     /**
      * bubble sorting bigger to small
      */
-    private static void descendingSorting() {
+    private static void bubbleSortDescending() {
 
         int[] intArray = {20, 35, 15, 7, 55, 1, 14};
 
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 
             for (int i = 0; i < lastUnsortedIndex; i++) {
-
                 if (intArray[i] < intArray[i + 1]) {
                     swap(intArray, i, i + 1);
                 }
@@ -33,19 +48,16 @@ public class Main {
     }
 
 
-
-
     /**
      * bubble sorting small to bigger
      */
-    private static void ascendingSorting() {
+    private static void bubbleSortAscending() {
 
-        int[] arrayInt = {5, 12, 26, 2, 45, 3};
+        int[] arrayInt = {20, 35, -15, 7, 55, 1, -22};
 
         for (int unsortList = arrayInt.length - 1; unsortList > 0; unsortList--) {
 
             for (int i = 0; i < unsortList; i++) {
-
                 if (arrayInt[i] > arrayInt[i + 1]) {
                     swap(arrayInt, i, i + 1);
                 }
@@ -56,7 +68,6 @@ public class Main {
             System.out.print(" " + arrayInt[i]);
         }
     }
-
 
 
     private static void swap(int[] array, int i, int j) {
