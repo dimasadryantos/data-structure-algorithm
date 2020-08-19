802707db6403535e9297648311380f9a78bc37b3
dimasadriyantos@gmail.com <dimasdz99>
2020-08-19 10:45:33 +0700

    bubble sort ascending (dari kecil ke besar)

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -2,7 +2,39 @@ package com.main;
 
 public class Main {
 
-    public static void main(String [] args){
-        System.out.println("HELLO FUCKING WORLD !!");
+    public static void main(String[] args) {
+
+
+        int[] intArray = {20, 35, 15, 7, 55, 1, 14};
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(" " + intArray[i]);
+        }
+
     }
+
+
+    private static void swap(int[] array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    //OUTPUT SHOULD BE : 1 7 14 15 20 35 55
+
 }
