9cc493839529a35a7a621821c9002ce46a6f45d0
dimasadriyantos@gmail.com <dimasdz99>
2020-08-20 10:38:59 +0700

    Insertion Sort Ascending

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,8 +3,26 @@ package com.main;
 public class Main {
 
     public static void main(String[] args) {
+        insertionSortsAscending();
+    }
 
 
+    private static void insertionSortsAscending() {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+
+            int newElement = intArray[firstUnsortedIndex];
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+            intArray[i] = newElement;
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(" " + intArray[i]);
+        }
     }
 
     private static void selectionShortAscending() {
@@ -21,7 +39,7 @@ public class Main {
             swap(intArray, largest, lastSortedIdex);
         }
 
-        for(int i =0;i < intArray.length;i++){
+        for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
     }
