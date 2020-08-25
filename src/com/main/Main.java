422e8f2d114ef0da974a7c57a0e0373f548146c5
dimasadriyantos@gmail.com <dimasdz99>
2020-08-25 13:10:47 +0700

    Quick Sort

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,8 +3,46 @@ package com.main;
 public class Main {
 
     public static void main(String[] args) {
+        int[] intArray1 = {20, 35, -15, 7, 55, 1, -22};
+
 
     }
+
+
+    public static void quickSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        quickSort(input, pivotIndex + 1, end);
+    }
+
+    //{20, 35, -15, 7, 55, 1, -22}
+    public static int partition(int[] input, int start, int end) {
+        // This is using the first element as the pivot
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+            // NOTE: empty loop body
+            while (i < j && input[--j] >= pivot) ;
+            if (i < j) {
+                input[i] = input[j];
+            }
+
+            // NOTE: empty loop body
+            while (i < j && input[++i] <= pivot) ;
+            if (i < j) {
+                input[j] = input[i];
+            }
+        }
+        input[j] = pivot;
+        return j;
+
+    }
+
 
     /**
      * Merge Sort Recursive
